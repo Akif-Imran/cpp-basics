6d039a7a4362ff52f1cc59e8da0bed23f6cc2e2d
Akif-Imran <raiden3322@gmail.com>
2024-08-17 00:53:32 +0500

    Add project files.

---
+++ cpp-basics.cpp
@@ -0,0 +1,12 @@
+﻿// cpp-basics.cpp : Defines the entry point for the application.
+//
+
+#include "cpp-basics.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
